4862792d0e45203917824ab7e1818698cc738d37
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-05-08 18:11:57 +0200

    ADD: script-vue

--- assets/script.js
+++ assets/script.js
@@ -43,120 +43,40 @@ const app = Vue.createApp({
 
 
         }
+    },
+
+    methods: {
+        nextImage() {
+            this.activeIndex++;
+            if (this.activeIndex >= this.arrImages.length) {
+                this.activeIndex = 0;
+            }
+        },
+        prevImage() {
+            this.activeIndex--;
+            if (this.activeIndex < 0) {
+                this.activeIndex = this.arrImages.length - 1;
+            }
+        },
+        currentImage(index) {
+            this.activeIndex = index;
+        }
+    },
+    created() {
+        setInterval(() => {
+            this.activeIndex++;
+            if (this.activeIndex >= this.arrImages.length) {
+                this.activeIndex = 0;
+            }
+        }, 3000);
     }
 });
+
+
 app.mount('.carousel');
 
 
 
-// setInterval(() => {
-//     title[activeIndex].classList.remove('active');
-// 	text[activeIndex].classList.remove('active');
-//     listHighlighted[activeIndex].classList.remove('active');
-//     listThumbs[activeIndex].classList.remove('active');
-   
-//     activeIndex++;
-//     if (activeIndex >= listHighlighted.length) {
-//         activeIndex = 0;
-//     }
-//     title[activeIndex].classList.add('active');
-// 	text[activeIndex].classList.add('active');
-//     listHighlighted[activeIndex].classList.add('active');
-//     listThumbs[activeIndex].classList.add('active');
-// }, 2000);
-
-// const containerHighlighted = document.querySelector('.highlighted');
-// const containerThumbs = document.querySelector('.thumbs');
-
-
-// for (let i = 0; i < arrayImage.length; i++) {
-// 	containerHighlighted.innerHTML += `
-    
-//         <img src="${arrayImage[i].image}" alt=""  class="${i == 0 ? 'active' : ''}">
-//         <div class="title ${i == 0 ? 'active' : ''}"> ${arrayImage[i].title} </div>
-//         <div class="text ${i == 0 ? 'active' : ''}"> ${arrayImage[i].text}  </div>`;
-        
-//         containerThumbs.innerHTML += `<img src="${arrayImage[i].image}" alt="" class="${i == 0 ? 'active' : ''}">`;
-// }
-
-
-// // selezionimo le immagini nell'html
-// const title = document.querySelectorAll('.title');
-// const text = document.querySelectorAll('.text');
-// const listHighlighted = document.querySelectorAll('.highlighted img');
-// // selezioniamo le miniature
-// const listThumbs = document.querySelectorAll('.thumbs img');
-// // selezioniamo i bottoni
-// const btnPrev = document.querySelector('.btn-prev');
-// const btnNext = document.querySelector('.btn-next');
-
-
-// // definito una variabile che rappresenta lo stato attuale del carosello
-// // cioe' l'indice dell'immagine attiva
-// let activeIndex = 0;
-
-// btnNext.addEventListener('click',
-// 	function() {
-// 		// dall'immagine attiva tolgo la classe active
-//         title[activeIndex].classList.remove('active');
-// 		text[activeIndex].classList.remove('active');
-//         listHighlighted[activeIndex].classList.remove('active');
-// 		listThumbs[activeIndex].classList.remove('active');
-// 		// settiamo il nuovo valore di active index
-// 		activeIndex++;
-// 		if (activeIndex >= listHighlighted.length) {
-// 			activeIndex = 0;
-// 		}
-// 		// alla nuova immagine attiva aggiungiamo la classe active
-//         title[activeIndex].classList.add('active');
-// 		text[activeIndex].classList.add('active');
-//         listHighlighted[activeIndex].classList.add('active');
-// 		listThumbs[activeIndex].classList.add('active');
-// 	}
-// );
-
-// btnPrev.addEventListener('click',
-// 	function() {
-// 		// dall'immagine attiva tolgo la classe active
-//         title[activeIndex].classList.remove('active');
-// 		text[activeIndex].classList.remove('active');
-//         listHighlighted[activeIndex].classList.remove('active');
-// 		listThumbs[activeIndex].classList.remove('active');
-       
-// 		// settiamo il nuovo valore di active index
-// 		activeIndex--;
-// 		if (activeIndex < 0) {
-// 			activeIndex = listHighlighted.length - 1;
-// 		}
-// 		// alla nuova immagine attiva aggiungiamo la classe active
-        
-//         title[activeIndex].classList.add('active');
-// 		text[activeIndex].classList.add('active');
-// 		listHighlighted[activeIndex].classList.add('active');
-// 		listThumbs[activeIndex].classList.add('active');
-// 	}
-// );
-
-// // ciclo per aggiungere gli event listeners alle miniature
-// for (let i = 0; i < listThumbs.length; i++) {
-// 	listThumbs[i].addEventListener('click',
-// 		function() {
-// 			console.log('cliccata la miniature in posizione ' + i)
-            
-//             title[activeIndex].classList.remove('active');
-//             text[activeIndex].classList.remove('active');
-// 			listHighlighted[activeIndex].classList.remove('active');
-// 			listThumbs[activeIndex].classList.remove('active');
-			
-//             activeIndex = i;
-            
-//             title[activeIndex].classList.add('active');
-// 		    text[activeIndex].classList.add('active');
-//             listHighlighted[activeIndex].classList.add('active');
-// 			listThumbs[activeIndex].classList.add('active');
-// 		}
-// 	)
-// }
 
 
 
